99c9051604f8b224b04ee9591b2e6ba8fddf59ca
CAMUS Paul <paul.camus@etu.sorbonne-universite.fr>
2025-12-19 10:41:04 +0100

    Ajout d'un 4eme moteur test avec des frottements pour comparer les courbes. Courbes colorées.

--- projet.py
+++ projet.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class MoteurCC:
-    def __init__(self, resistance=1, inductance=0, fcem=0.01, couple=0.01, inertie=0.01, visc=0.1):
+    def __init__(self, resistance=1, inductance=0, fcem=0.01, couple=0.01, inertie=0.01, visc=0.1, name="MoteurCC", color=None):
 
+        # param généraux
+        self.name = name
+        self.color = color
+
+        # paramètres électriques et mécaniques du moteur
         self.R = resistance
         self.L = inductance
         self.Ke = fcem
@@ -11,16 +16,16 @@ class MoteurCC:
         self.I = inertie
         self.Nu = visc
 
-        # Variables d'état
+        # états du moteur
         self.Um = 0
         self.i = 0
         self.omega = 0
         self.theta = 0
 
-        # Liste pour l'historique (pour plot)
+        # Liste pour l'historique
         self.time_history = []
         self.speed_history = []
-        self.current_time = 0
+        self.temps = 0
 
         # ajout des perturbations externes
         self.J_charge = 0.0
@@ -28,19 +33,15 @@ class MoteurCC:
         self.C_ext = 0.0 
 
 
-    def __str__(self):        return (f"MoteurCC(R={self.R}, L={self.L}, Ke={self.Ke}, "
-                f"Kt={self.Kt}, I={self.I}, Nu={self.Nu})")
-    def __repr__(self):
-        return str(self)
+    def __str__(self):return (f"MoteurCC(R={self.R}, L={self.L}, Ke={self.Ke}, Kc={self.Kc}, I={self.I}, Nu={self.Nu})")
+    def __repr__(self):return str(self)
     
-    def setLoad(self, inertie_charge, frottement_charge=0):
+    def setCharge(self, inertie_charge, frottement_charge=0):
         self.J_charge = inertie_charge
         self.f_charge = frottement_charge
 
-    def setExternalTorque(self, couple_resistif):
-        self.C_ext = couple_resistif
-    def setVoltage(self, V):
-        self.Um = V
+    def setCoupleExterne(self, couple_resistant):self.C_ext = couple_resistant
+    def setVoltage(self, V):self.Um = V
     
     def getPosition(self): return self.theta
     def getSpeed(self): return self.omega
@@ -87,50 +88,56 @@ class MoteurCC:
         self.theta += self.omega * step
         
         # sauvegarde pour l'affichage
-        self.current_time += step
-        self.time_history.append(self.current_time)
+        self.temps += step
+        self.time_history.append(self.temps)
         self.speed_history.append(self.omega)
 
-    def plot(self):
+    def plot(self, modele_theorique=False):
+        if self.color is None:
+            # renvoie un tableau avec 3 valeurs aléatoires entre 0 et 1 pour définir aleatoirement une couleur
+            self.color = np.random.rand(3,)
+
         t = np.array(self.time_history)
         vitesses = np.array(self.speed_history)
 
-        plt.plot(t, vitesses, label='Simulation (Numérique)', linewidth=2)
+        plt.plot(t, vitesses, color=self.color, label=self.name, linewidth=2)
 
-        # courbe issue de la théorie avec L=0
-        # Omega(t) = K * U * (1 - exp(-t/tau))
-        deno = self.R * self.Nu + self.Kc * self.Ke
-        
-        if deno != 0:
-            K = self.Kc / deno
-            tau = (self.R * self.I) / deno
+        # pour éviter de tracer la courbe théorique dès qu'on plot
+        if modele_theorique:
+            # courbe issue de la théorie avec L=0
+            # Omega(t) = K * U * (1 - exp(-t/tau))
+            deno = self.R * self.Nu + self.Kc * self.Ke
             
-            vitesse_theo = K * self.Um * (1 - np.exp(-t / tau))
+            if deno != 0:
+                K = self.Kc / deno
+                tau = (self.R * self.I) / deno
+                
+                vitesse_theo = K * self.Um * (1 - np.exp(-t / tau))
 
-            plt.plot(t, vitesse_theo, 'r--', label='Théorie (Analytique L=0)', linewidth=2)
-
-        plt.title(f"Réponse indicielle d'un moteur à courant continu")
-        plt.xlabel("Temps (s)")
-        plt.ylabel("Vitesse de rotation (rad/s)")
-        plt.grid()
-        plt.legend()
+                plt.plot(t, vitesse_theo, 'r--', label='Modèle analytique', linewidth=2)
         
 if __name__ == "__main__":
-    plt.figure(figsize=(10, 6))
+
+    plt.figure(figsize=(30, 20))
 
     # moteur 1 sans charge
-    m1 = MoteurCC()
+    m1 = MoteurCC(name="Moteur sans charge")
     m1.setVoltage(1)
 
-    # moteur 2 avec une grosse inertie de charge
-    m2 = MoteurCC()
+    # moteur 2 avec une inertie de charge et des frottements supplémentaires
+    m2 = MoteurCC(name="Moteur avec charge")
     m2.setVoltage(1)
-    m2.setLoad(inertie_charge=0.05)
+    m2.setCharge(inertie_charge=0.05)
     
     # moteur 3 avec un couple extérieur résistant
-    m3 = MoteurCC()
+    m3 = MoteurCC(name="Moteur avec couple extérieur")
     m3.setVoltage(1)
-    m3.setExternalTorque(couple_resistif=0.0005)
+    m3.setCoupleExterne(couple_resistant=0.0005)
+
+    # moteur 4 avec sans inertie de charge mais avec des frottements supplémentaires
+    m4 = MoteurCC(name="Moteur avec frottements supplémentaires")
+    m4.setVoltage(1)
+    m4.setCharge(inertie_charge=0, frottement_charge=0.02)
 
     t = 0
     step = 0.01
@@ -140,43 +147,22 @@ if __name__ == "__main__":
         m1.simule(step)
         m2.simule(step)
         m3.simule(step)
+        m4.simule(step)
         t += step
 
-    m1.plot()
+    m1.plot(True)
     m2.plot()
     m3.plot()
+    m4.plot()
     
-    plt.title("Influence de la charge et du couple extérieur")
+    plt.title("Réponse indicielle en vitesse de différents moteurs à courant continu")
     plt.xlabel("Temps (s)")
     plt.ylabel("Vitesse (rad/s)")
     plt.grid()
     plt.legend()
     plt.show()
 
-if __name__ == "__main__":
-    m = MoteurCC()
-    t = 0
-    step = 0.01
-    temps = [t]
-    while t<2 :
-        t=t+step
-        temps.append(t)
-        m.setVoltage(1)
-        m.simule(step)
-    plt.figure(figsize=(50,36))
-    m.plot()
-    plt.show()
 
-"""mécaniques
-• Entrées : Tension, charge (inertie ajoutée), couple extérieur
-résistif, viscosité
-• Sorties : Courant, couple, vitesse, position
-méthodes :
-• _init_, _str_, _repr_
-• setVoltage(V)
-• getPosition(), getSpeed(), getTorque(), getIntensity()
-• simule(step)
-• plot()"""
 
 
 """Classe ControlPID_vitesse :
