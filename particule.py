ef10be15aef62ef251369ee48f7bb6eb2a3b7cfa
Paul CAMUS <paul.camus@etu.sorbonne-universite.fr>
2025-12-28 17:45:25 +0100

    Création d'une classse pendule à compléter plus tard pour comparer la fréquence propre pour la phase de validation de la barre 2D.

--- particule.py
+++ particule.py
@@ -247,25 +247,77 @@ class Glissiere:
             pygame.draw.line(screen, (80, 80, 80), p0, p1, 3)
             
 
+class Pendule:
+    def __init__(self, attache_pos=v(0, 0, 0), longueur=1.0, angle_init=0.0):
+        from math import cos,sin,sqrt
+
+        self.ancre = Particule(fixed=True, position=attache_pos, color=(0, 0, 0))
+        self.masse = Particule(position=attache_pos + longueur*v(sin(angle_init), -cos(angle_init), 0), color=(255, 0, 0))
+        self.longueur = longueur
+        self.angle = [angle_init]
+        self.vitesse = []
+        self.fil = Fil(P0=self.ancre, P2=self.masse)
+
+    def gameDraw(self, screen, scale):
+        self.ancre.gameDraw(screen, scale)
+        self.masse.gameDraw(screen, scale)
+        self.fil.drawArea(screen, scale)
+
+    def getAngle(self):
+        return self.angle[-1]
+    
+    def simule(self, step):
+        self.fil.setForce(self.masse)
+
+        self.angle.append()
+        self.vitesse.append(self.masse.getSpeed())
+
+
+    def plot(self):
+        pass
+
+if __name__ == "__main__":
+    def run_glissiere():
+        from types import MethodType
+
+        uni = Univers(name="Glissière", game=True)
+
+        perle = Particule(position=v(20, 50, 0), masse=2.0, name='Perle')
+        uni.addParticules(perle)
+
+        gravite = Gravity(v(0, -9.81, 0))
+
+        rail = Glissiere(origine=v(50, 50, 0), targets=[perle], direction=v(1, -0.5, 0), k=8000, c=100, name='Rail')
+
+        def gameInteraction(self, events, keys):
+            if keys[pygame.K_SPACE]:
+                perle.applyForce(v(50, 0, 0))
+            
+
+        uni.addGenerators(gravite, rail)
+        uni.gameInteraction = MethodType(gameInteraction, uni)
+
+        
+        uni.simulateRealTime()
+
 if __name__ == "__main__":
     from types import MethodType
 
-    uni = Univers(name="Glissière", game=True)
-
-    perle = Particule(position=v(20, 50, 0), masse=2.0, name='Perle')
-    uni.addParticules(perle)
+    uni = Univers(name="Pendule Simple", game=True, dimensions=(50, 50))
+    p = Pendule(attache_pos=v(25, 40, 0), longueur=10.0, angle_init=np.pi/4)
+    uni.addParticules(p.ancre, p.masse)
 
     gravite = Gravity(v(0, -9.81, 0))
 
-    rail = Glissiere(origine=v(50, 50, 0), targets=[perle], direction=v(1, -0.5, 0), k=8000, c=100, name='Rail')
-
     def gameInteraction(self, events, keys):
-        if keys[pygame.K_SPACE]:
-             perle.applyForce(v(50, 0, 0))
-        
+        vitesse_deplacement = 0.1
+        if keys[pygame.K_LEFT]:p.ancre.position[-1].x -= vitesse_deplacement
+        if keys[pygame.K_RIGHT]:p.ancre.position[-1].x += vitesse_deplacement
+        if keys[pygame.K_UP]:p.ancre.position[-1].y += vitesse_deplacement
+        if keys[pygame.K_DOWN]:p.ancre.position[-1].y -= vitesse_deplacement
 
-    uni.addGenerators(gravite, rail)
+
+    uni.addGenerators(gravite, p.fil)
     uni.gameInteraction = MethodType(gameInteraction, uni)
 
-    
     uni.simulateRealTime()
